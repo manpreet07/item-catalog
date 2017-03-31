21a0950af4d659ecbabcb71fc444aa89ab6c4bcf
Manpreet Singh <manpreet.singh2@hpe.com>
2017-03-31 12:43:48 -0400

    fixed facebook login

--- application.py
+++ application.py
@@ -19,8 +19,7 @@ from database_setup import Base, Category, Item, User
 app = Flask(__name__)
 
 
-CLIENT_ID = json.loads(
-    open('client_secrets.json', 'r').read())['web']['client_id']
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Category"
 
 # Connect to Database and create database session
@@ -102,27 +101,35 @@ def fbconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = request.data
-    print ("access token received %s") % access_token
+    # print ("access token received %s") % access_token
 
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
-        'web']['app_id']
-    app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
         app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
     # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.5/me"
+    # userinfo_url = "https://graph.facebook.com/v2.8/me"
+
     # strip expire tag from access token
     token = result.split("&")[0]
+    token_data = json.loads(token)
+    t = token_data["access_token"]
 
-    url = 'https://graph.facebook.com/v2.5/me?%s&fields=name,id,email' % token
+    # print ("Token is %s") % t
+
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % t
+
+    # print ("URL is %s") % url
+
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    # print "url sent for API access:%s"% url
-    # print "API JSON result: %s" % result
+
+    # print ("url sent for API access:%s") % url
+    # print ("API JSON result: %s") % result
+
     data = json.loads(result)
     login_session['provider'] = 'facebook'
     login_session['username'] = data["name"]
@@ -132,11 +139,11 @@ def fbconnect():
     # The token must be stored in the login_session in order to properly
     # logout, let's strip out the information before the equals sign in our
     # token
-    stored_token = token.split("=")[1]
-    login_session['access_token'] = stored_token
+    # stored_token = token.split("=")[1]
+    login_session['access_token'] = t
 
     # Get user picture
-    url = 'https://graph.facebook.com/v2.5/me/picture?%s&redirect=0&height=200&width=200' % token
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % t
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
